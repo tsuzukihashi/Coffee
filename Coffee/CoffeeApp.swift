fd64b88c2a1996642655cb623528b2b99f4a6563
tsuzukihashi <tsudukihashi0817@gmail.com>
2020-09-22 00:04:56 +0900

    first commit

---
+++ Coffee/CoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeApp.swift
+//  Coffee
+//
+//  Created by Ryo Tsuzukihashi on 2020/09/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
